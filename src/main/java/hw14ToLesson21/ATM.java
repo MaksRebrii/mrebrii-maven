bc959100d6c5eb1131e3e480b2df1890d32df89c
Maksymilian Rebrii <maksim.rebriy@gmail.com>
2021-10-24 22:49:47 +0300

    complete task v2.0

--- src/main/java/hw14ToLesson21/ATM.java
+++ src/main/java/hw14ToLesson21/ATM.java
@@ -1,28 +1,47 @@
 package hw14ToLesson21;
 
+import java.util.Scanner;
+
 public class ATM {
+    private static final int MIN_MONEY = 1;
+    private final int maximumTopUpAmount;
+    private final int maximumWithdrawalAmount;
     private final Object monitor;
     private int sum;
 
-    public ATM(int sum) {
+    public ATM(int sum, int maximumTopUpAmount, int maximumWithdrawalAmount) {
         this.sum = sum;
+        this.maximumTopUpAmount = maximumTopUpAmount;
+        this.maximumWithdrawalAmount = maximumWithdrawalAmount;
         this.monitor = new Object();
     }
 
-    public void getMoney(int amountMoneyTakenOff) {
+    public void getMoney() {
+        int amountMoneyTakenOff = randomAmountOfMoney(maximumWithdrawalAmount);
         synchronized (monitor) {
             if (this.sum >= amountMoneyTakenOff) {
                 this.sum -= amountMoneyTakenOff;
                 System.out.printf("%s - снятие - %d%n", Thread.currentThread().getName(), amountMoneyTakenOff);
-            } else
-                System.out.printf("В банкомате недостаточно денег для снятия  %s%n", Thread.currentThread().getName());
+            } else {
+                Scanner scanner = new Scanner(System.in);
+                do {
+                    System.out.printf("В банкомате недостаточно денег для снятия  %s%n ", Thread.currentThread().getName());
+                    System.out.printf("Введите сумму меньшую чем %d: ", amountMoneyTakenOff);
+                    amountMoneyTakenOff = scanner.nextInt();
+                } while (this.sum < amountMoneyTakenOff);
+            }
         }
     }
 
-    public void putMoney(int amountMoneyReceived) {
+    public void putMoney() {
+        int amountMoneyReceived = randomAmountOfMoney(maximumTopUpAmount);
         synchronized (monitor) {
             this.sum += amountMoneyReceived;
             System.out.printf("%s - пополнение - %d%n", Thread.currentThread().getName(), amountMoneyReceived);
         }
+    }
+
+    private int randomAmountOfMoney(int topBorder){
+        return (int)(Math.random() * (topBorder - MIN_MONEY) + MIN_MONEY);
     }
 }
--- src/main/java/hw14ToLesson21/Main.java
+++ src/main/java/hw14ToLesson21/Main.java
@@ -1,13 +1,29 @@
 package hw14ToLesson21;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+
+    private static final int RANDOM_BOUND = 10;
+    private static final int OPERATION_AMOUNT = 5;
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(3);
+    private static final Random RANDOM = new Random();
+
     public static void main(String[] args) {
-        ATM atm = new ATM(10000);
+        ATM atm = new ATM(100_000, 10_000, 100_000);
 
+        for (int i = 0; i < OPERATION_AMOUNT; i++) {
+            executorService.execute(() -> {
+                int rand = RANDOM.nextInt(RANDOM_BOUND);
+                if (rand % 2 == 0)
+                    atm.getMoney();
+                else
+                    atm.putMoney();
+            });
+        }
 
-        new Thread(() -> atm.getMoney(4000)).start();
-        new Thread(() -> atm.putMoney(1000)).start();
-        new Thread(() -> atm.getMoney(10000)).start();
     }
 
 }
